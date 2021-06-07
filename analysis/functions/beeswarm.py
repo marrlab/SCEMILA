d500f4ab38765c704159bde314c44bfc82f7f944
matthias.hehr <matthias.hehr@helmholtz-muenchen.de>
2021-06-07 15:05:45 +0200

    modified beeswarm

--- analysis/functions/beeswarm.py
+++ analysis/functions/beeswarm.py
@@ -2,12 +2,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-def beeswarm_coordinates(df, val_col):
+def beeswarm_coordinates(df, val_col, xscale="log", figsize=(9,9), pointsize=7):
     '''small hack: don't show image, but use seaborn to calculate coordinates.
     Could not find a solution performing as well as the seaborn implementation'''
-    fig, ax = plt.subplots(figsize=(9, 9))
-    ax.set(xscale="log")
-    fig = sns.swarmplot(data=df, x=val_col, size=7)
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.set(xscale=xscale)
+    fig = sns.swarmplot(data=df, x=val_col, size=pointsize)
     # get precise data coordinates
     x, y = np.array(ax.collections[0].get_offsets()).T
     df = df.sort_values(by=val_col, ascending=True)
